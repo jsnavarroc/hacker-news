8d9a14f5fc92a9e5306e856ad623c7b11edc9aff
Johan Navarro <jcioweb@gmail.com>
2023-02-05 12:00:08 -0500

    feat: :dizzy: input animation is added
    when you click on the input options button load the options
    
    nothing breaks

--- src/components/organims/InputList.tsx
+++ src/components/organims/InputList.tsx
@@ -1,11 +1,35 @@
+import {useState} from 'react'
+import { angular, react, vue } from "../../assets/icons/Icons";
+
 import FilterSearch from "../molecules/FilterSearch"
 import OptionsInput from "../molecules/OptionsInput"
+import { OptionI } from '../types';
 
 const InputList = () => {
+
+    const [isOpen, setIsOpen] = useState(false);
+    const options:OptionI[] = [
+      {
+        icon: angular,
+        text: "Angular",
+      },
+      {
+        icon: react,
+        text: "React",
+      },     
+      {
+        icon: vue,
+        text: "Vue",
+      },
+    ];
+    const onClick = () => {
+        setIsOpen(!isOpen)
+    }
+
     return (
         <>
-         <FilterSearch/>
-         <OptionsInput/>
+         <FilterSearch onClick={onClick}/>
+         <OptionsInput options={options} isOpen={isOpen}/>
         </>
     )
 }
